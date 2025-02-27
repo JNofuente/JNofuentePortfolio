e273943b43f19d4bb63b06e75457b76ebc096680
JNofuente <159685666+JNofuente@users.noreply.github.com>
2025-02-27 14:36:32 +0800

    Update skills.js

--- skills.js
+++ skills.js
@@ -1,12 +1,12 @@
 function containerClicked(containerNumber) {
     const links = {
         1: 'Compskill.html',
-        2: '/Editing.html',
-        3: "Time.html",
-        4: "/ITknowledge.html",
-        5: '.Solving.html',
-        6: ".Communication.html",
-        7: "/.Thinking.html"
+        2: 'Editing.html',
+        3: 'Time.html',
+        4: 'ITknowledge.html',
+        5: 'Solving.html',
+        6: 'Communication.html',
+        7: 'Thinking.html'
     };
     window.location.href = links[containerNumber];
 }
