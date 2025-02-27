f7fe3298e5514825d3a1390bb10b3714f41a8a5e
JNofuente <159685666+JNofuente@users.noreply.github.com>
2025-02-27 12:43:51 +0800

    Update about.js

--- about.js
+++ about.js
@@ -5,11 +5,11 @@ let playPauseBtn = document.getElementById("play-pause-btn");
 function toggleAudio() {
     if (audio.paused) {
         audio.play();
-        playPauseBtn.src = "/images/pauseicon.png";
+        playPauseBtn.src = "pauseicon.png";
         playPauseBtn.alt = "Pause";
     } else {
         audio.pause();
-        playPauseBtn.src = "/images/playicon.png";
+        playPauseBtn.src = "playicon.png";
         playPauseBtn.alt = "Play";
     }
 }
@@ -25,7 +25,7 @@ function seekAudio() {
 }
 
 function resetAudioControls() {
-    playPauseBtn.src = "/images/playicon.png";
+    playPauseBtn.src = "playicon.png";
     playPauseBtn.alt = "Play";
     progressBar.value = 0;
 }
